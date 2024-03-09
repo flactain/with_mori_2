ac210fcae72bca954075a86b6385c4cef07a9b52
moriyamato <flactly@gmail.com>
2024-03-09 17:40:04 +0900

    DB接続をベタ書きするのではなく、環境変数を使用するように変更。

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -5,20 +5,30 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import model.Bean;
 
 public class Dao {
 	public Bean Connection(int id) {
 
+//EDIT 24-03-09 Y.Mori  DBの接続情報ベタ書き→コンテナの環境変数を使用するように変更 
 				//DB接続用定数
-				String DATABASE_NAME = "sample";
-				String PROPATIES = "?characterEncoding=UTF-8"; //timezone指定はエラー吐くらしい
+				//String DATABASE_NAME = "sample";
+				//String PROPATIES = "?characterEncoding=UTF-8"; //timezone指定はエラー吐くらしい
 //				String URL = "jdbc:mysql://localhost:3306/sample"; //+ DATABASE_NAME;// + PROPATIES;
-				String URL = "jdbc:mysql://db:3306/" + DATABASE_NAME + PROPATIES;
+				//String URL = "jdbc:mysql://db:3306/" + DATABASE_NAME + PROPATIES;
 				//DB接続用・ユーザ定数
-				String USER = "root";
-				String PASS = "root";
+				//String USER = "root";
+				//String PASS = "root";
+				String DATABASE_NAME = System.getenv("DB_NAME");
+				String URL = System.getenv("DB_URL");
+				String PROPATIES = System.getenv("DB_PROPATIES");
+				String USER =System.getenv("DB_USER");
+				String PASS=System.getenv("DB_PASS");	
+				
+				URL = URL + DATABASE_NAME ;//+ PROPATIES;
+				
 				Connection connection = null;
 				Bean bean= null;
 		
